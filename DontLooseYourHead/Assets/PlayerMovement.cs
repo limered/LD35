b6f9c25c2814c8e0e1d4b1806a540b57006c7f91
limered <me@limered.de>
2016-04-17 00:21:52 +0200

    Player Handle Picking done (mouse)

--- DontLooseYourHead/Assets/PlayerMovement.cs
+++ DontLooseYourHead/Assets/PlayerMovement.cs
@@ -1,31 +1,63 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+    private GameObject grabbedHandle = null;
+    private Vector3 startPos = Vector3.zero;
+    private float movementplaneDistance = 0;
 
     // Use this for initialization
-    void Start () {
-    
+    private void Start()
+    {
     }
-    
+
     // Update is called once per frame
-    void Update () {
-
-
-        if (Input.GetMouseButtonDown(0))
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && grabbedHandle)
         {
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var rayPoint = ray.GetPoint(movementplaneDistance);
+            var newPosition = new Vector3(rayPoint.x, rayPoint.y, startPos.z);
+            grabbedHandle.transform.position = newPosition;
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var rayPoint = ray.GetPoint(transform.position.z - Camera.main.transform.position.z);
 
-            var handles = GameObject.FindGameObjectsWithTag("Line");
+            var handles = GameObject.FindGameObjectsWithTag("Handle");
             if (handles != null && handles.Length > 0)
             {
                 var nearest = handles[0];
+                var minDist = GetDistanceSq(nearest, rayPoint);
                 foreach (var handle in handles)
                 {
-                    
+                    var newDistance = GetDistanceSq(handle, rayPoint);
+                    if (newDistance < minDist)
+                    {
+                        minDist = newDistance;
+                        nearest = handle;
+                    }
+                }
+
+                if (nearest)
+                {
+                    grabbedHandle = nearest;
+                    movementplaneDistance = nearest.transform.position.z - Camera.main.transform.position.z;
+                    startPos = nearest.transform.position;
                 }
             }
         }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            grabbedHandle = null;
+        }
+    }
 
+    private float GetDistanceSq(GameObject handle, Vector3 rayPoint)
+    {
+        var diff = handle.transform.position - rayPoint;
+        return new Vector2(diff.x, diff.y).SqrMagnitude();
     }
 }

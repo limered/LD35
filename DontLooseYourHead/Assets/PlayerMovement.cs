36c429d58576c22617968a5716f1fdf58baed804
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2016-04-16 21:57:38 +0200

    temp

---
+++ DontLooseYourHead/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            var handles = GameObject.FindGameObjectsWithTag("Line");
+            if (handles != null && handles.Length > 0)
+            {
+                var nearest = handles[0];
+                foreach (var handle in handles)
+                {
+                    
+                }
+            }
+        }
+
+    }
+}

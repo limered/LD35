3f18a04e94084b0683cf51d8467eec2c84de79df
ryupold <ryupold@gmail.com>
2016-04-19 00:54:47 +0200

    added HUD

---
+++ DontLooseYourHead/Assets/Code/Behaviours/HUD.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Globalization;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text scoreText;
+    [SerializeField]
+    private Text nextScoreText;
+
+    [SerializeField]
+    private Text endScoreText;
+
+    [SerializeField]
+    private GameObject gameOverPanel;
+
+    private float score;
+    private float nextScore;
+
+    public float Score
+    {
+        get
+        {
+            return score;
+        }
+        set
+        {
+            score = value;
+            scoreText.text = score.ToString(CultureInfo.InvariantCulture);
+            endScoreText.text = score.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+
+    public float NextScore
+    {
+        get
+        {
+            return nextScore;
+        }
+        set
+        {
+            nextScore = value;
+            nextScoreText.text = "+ "+nextScore.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+
+    public HUD()
+    {
+        IoC.RegisterSingleton(this);
+    }
+
+
+    public void ShowGameOverScreen()
+    {
+        gameOverPanel.SetActive(true);
+    }
+
+
+    void Start()
+    {
+        gameOverPanel.SetActive(false);
+    }
+
+    private float test;
+    void Update()
+    {
+        test += Time.deltaTime;
+        if (test>5)
+        {
+            ShowGameOverScreen();
+        }
+    }
+}
--- DontLooseYourHead/Assets/Code/Behaviours/MainMenuView.cs
+++ DontLooseYourHead/Assets/Code/Behaviours/MainMenuView.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainMenuView : MonoBehaviour {
+public class MainMenuView : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
 }
